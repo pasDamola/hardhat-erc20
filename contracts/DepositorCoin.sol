a514a888a07f819e7d4aa2c4dbb66a4d94ca81c9
Oyindamola Oyetunmibi <oyindamolaoyetunmibi@Oyindamolas-MacBook-Pro.local>
2022-05-19 01:28:25 +0100

    full stable coin implementation

--- contracts/DepositorCoin.sol
+++ contracts/DepositorCoin.sol
@@ -6,10 +6,10 @@ import { ERC20 } from "./ERC20.sol";
 
 contract DepositorCoin is ERC20 {
     address public owner;
-    constructor() ERC20("DepositorCoin", "DPS") {
+    constructor() ERC20("DepositorCoin", "DPC") {
         owner = msg.sender;
     }
-    
+
     function mint(address to, uint256 amount) external {
         require(msg.sender == owner, "DPC: Only owner can mint");
         _mint(to, amount);
